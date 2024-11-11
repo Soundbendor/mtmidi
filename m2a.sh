633da332920306bf95ceb206be2c9f8d271c4e62
derekxkwan <derek.x.kwan@gmail.com>
2024-11-10 20:35:43 -0800

    fix argt

--- m2a.sh
+++ m2a.sh
@@ -9,6 +9,6 @@ OUT_DIR=${1:-$OUT_DEFAULT}
 for f in $IN_DIR/*mid;
 do 
     F_NAME=$(basename ${f});
-    OUT_FILE="${OUT_DEFAULT}/${F_NAME%.*}.wav"
+    OUT_FILE="${OUT_DIR}/${F_NAME%.*}.wav"
     cargo run --manifest-path "${SYNTOML}" -- "${SOUNDFONT}" "${f}" "${OUT_FILE}";
 done;
