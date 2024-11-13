08ab265cbd543231f9184668e1c36586c3996af0
mrufrufin <derek.x.kwan@gmail.com>
2024-11-12 23:52:07 -0800

    flip args

--- m2a.sh
+++ m2a.sh
@@ -2,10 +2,10 @@
 
 SYNTOML="../syntheory/midi2audio/Cargo.toml"
 IN_DEFAULT="midi"
-IN_DIR=${2:-$IN_DEFAULT}
+IN_DIR=${1:-$IN_DEFAULT}
 OUT_DEFAULT="/media/dxk/TOSHIBA EXT/wav"
 SOUNDFONT=TimGM6mb.sf2
-OUT_DIR=${1:-$OUT_DEFAULT}
+OUT_DIR=${2:-$OUT_DEFAULT}
 for f in $IN_DIR/*mid;
 do 
     F_NAME=$(basename ${f});
