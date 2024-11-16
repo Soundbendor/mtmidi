b77fba4d4b47630c41650f056fcd0233b02d6468
mrufrufin <derek.x.kwan@gmail.com>
2024-11-15 16:53:02 -0800

    pathing

--- m2a.sh
+++ m2a.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script
+CURDIR=$(dirname "$(realpath "$0")");
 SYNTOML="../syntheory/midi2audio/Cargo.toml"
-IN_DEFAULT="midi"
+IN_DEFAULT="$CURDIR/midi"
 IN_DIR=${1:-$IN_DEFAULT}
 OUT_DEFAULT="/media/dxk/TOSHIBA EXT/wav"
-SOUNDFONT=TimGM6mb.sf2
+SOUNDFONT="$CURDIR/TimGM6mb.sf2"
 OUT_DIR=${2:-$OUT_DEFAULT}
 for f in $IN_DIR/*mid;
 do 
