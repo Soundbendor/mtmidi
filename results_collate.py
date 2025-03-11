5e4c5ff568b140cba3b26ac404a7a8d4a2cbb4e4
derekxkwan <derek.x.kwan@gmail.com>
2025-03-11 01:05:48 -0700

    add timestamp

--- results_collate.py
+++ results_collate.py
@@ -1,12 +1,13 @@
-import csv, os, sys
+import csv, os, sys, time
 
-outfname = 'combined.csv'
+cur_time = int(time.time() * 1000)
+outfname = f'{cur_time}-combined.csv'
 outf = open(os.path.join('res_csv',outfname), 'w')
 csvw = csv.writer(outf)
 keys = None
 first_file = False
 for fi,f in enumerate(os.listdir('res_csv')):
-    if f == outfname:
+    if 'combined' in f:
         continue
     iptf = open(os.path.join('res_csv', f), 'r')
     csvr = csv.DictReader(iptf)
