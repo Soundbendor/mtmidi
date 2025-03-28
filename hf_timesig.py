56bafbd9d85bd534915cda4febcc033a1c3c1691
derekxkwan <derek.x.kwan@gmail.com>
2025-03-28 00:42:38 -0700

    bugfix

--- hf_timesig.py
+++ hf_timesig.py
@@ -1,4 +1,4 @@
 timesig_to_idx ={"(3, 8)":0, "(6, 8)":1, "(9, 8)":2, "(12, 8)":3, "(2, 4)":4, "(3, 4)":5, "(4, 4)":6, "(2, 2)" :7} 
 idx_to_timesig = {y:x for (x,y) in timesig_to_idx.items()}
-num_timesig = len(sig_to_idx.keys())
+num_timesig = len(timesig_to_idx.keys())
 
