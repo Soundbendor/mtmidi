847304ec4c608343929326ccf40eeba87592d642
derekxkwan <derek.x.kwan@gmail.com>
2025-08-14 22:17:55 -0700

    add layer idx

--- mtmidi_cka_slurm.py
+++ mtmidi_cka_slurm.py
@@ -4,6 +4,34 @@ import argparse
 import util as UM
 import os, time, subprocess
 
+def get_layer_idxs(idxarr, emb_type, emb1 = True):
+    li_arr = []
+    cur_shape = UM.get_embedding_shape(emb_type)
+    cur_emblayers = cur_shape[0]
+    if len(idxarr) > 1:
+        first_idx = max(0, idxarr[0])
+        last_idx = idxarr[-1]
+        if last_idx >= cur_emblayers:
+            old_last_idx = last_idx
+            last_idx = cur_emblayers - 1
+            e_str = 'embedding_type1'
+            if emb1 == False:
+                e_str = 'embedding_type2'
+            print(f'changing last_idx for {e_str} from {old_last_idx} to {last_idx}')
+        li_arr = list(range(first_idx, last_idx+1))
+    else:
+        cur_idx = max(0,idxarr[0])
+        if cur_idx >= cur_emblayers:
+            old_cur_idx = cur_idx
+            cur_idx = cur_emblayers - 1
+            e_str = 'embedding_type1'
+            if emb1 == False:
+                e_str = 'embedding_type2'
+            print(f'changing cur_idx for {e_str} from {old_cur_idx} to {cur_idx}')
+        li_arr = [cur_idx]
+    return li_arr
+    
+
 
 if __name__ == "__main__":
     #### arg parsing
@@ -35,12 +63,18 @@ if __name__ == "__main__":
     start_time = str(int(time.time() * 1000))
     ds_abbrev = UM.dataset_abbrev[args.dataset]
     for embedding_type1 in args.embedding_types1:
+
+       
+        li_arr1 = get_layer_idxs(args.layer_idxs1, embedding_type1, emb1 = True)
+        
         for embedding_type2 in args.embedding_types2:
             same_emb_type = embedding_type1.strip() == embedding_type2.strip()
             emb_abbrev1 = UM.model_abbrev[embedding_type1]
             emb_abbrev2 = UM.model_abbrev[embedding_type2]
-            for layer_idx1 in args.layer_idxs1:
-                for layer_idx2 in args.layer_idxs2:
+            li_arr2 = get_layer_idxs(args.layer_idxs2, embedding_type2, emb1 = False)
+      
+            for layer_idx1 in li_arr1:
+                for layer_idx2 in li_arr2:
                     same_li = layer_idx1 == layer_idx2
                     if not (same_emb_type and same_li):
                         slurm_strarr = ["#!/bin/bash", f"#SBATCH -p {args.partition}",f"#SBATCH --mem={args.ram_mem}G", f"#SBATCH --gres=gpu:{args.gpus}", "#SBATCH -t 1-00:00:00", f"#SBATCH --job-name={ds_abbrev}_{emb_abbrev1}{emb_abbrev2}cka", "#SBATCH --export=ALL", f"#SBATCH --output=/nfs/guille/eecs_research/soundbendor/kwand/slurm_out/{ds_abbrev}{emb_abbrev1}{emb_abbrev2}cka-%j.out", ""]
