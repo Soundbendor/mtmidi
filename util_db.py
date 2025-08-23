bfafb6bbeef4afba0009b16848a748f378e301a0
derekxkwan <derek.x.kwan@gmail.com>
2025-08-23 01:27:02 -0700

    add wrapper func

--- util_db.py
+++ util_db.py
@@ -25,9 +25,19 @@ def get_best_trial_id_val(conn):
 def get_best_trial_params(conn):
     best_row = get_best_trial_id_val(conn)
     best_id = best_row['trial_id'][0]
+    best_val = best_row['value'][0]
     best_params = pl.read_database(query = f'select param_name,param_value from trial_params where trial_id={best_id}', connection=conn)
-    return best_params
+    return (best_params, best_val)
 
 def close_dbconn(conn):
     conn.close()
+
+def get_best_params(cur_ds, cur_embtype, pfix=5):
+    conn = get_dbconn(cur_ds, cur_embtype, pfix=pfix)
+    (best_params, best_val) = get_best_trial_params(conn)
+    close_dbconn(conn)
+    param_dict = {x['param_name']: x['param_value'] for x in best_params.to_dicts()}
+    #param_dict['best_value'] = best_val
+    return (param_dict, best_val)
+
 
