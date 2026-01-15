72c73680542b68450ee85354c2acb3228e672ab3
Derek Kwan <dkwan6@gatech.edu>
2026-01-15 14:22:46 -0800

    add

--- reorg_to_folds.py
+++ reorg_to_folds.py
@@ -3,6 +3,7 @@ import polars as pl
 import util as UM
 import sys
 
+mv_test = True
 wavdir = '/nfs/hpc/share/kwand/wav'
 testdir = '/nfs/hpc/share/kwand/test'
 dataset = 'polyrhythms'
@@ -15,7 +16,7 @@ csvdir = UM.by_projpath(subpath='csv',make_dir = False)
 csvfile = os.path.join(csvdir, f'{dataset}-folds.csv')
 df = pl.read_csv(csvfile)
 
-os.mkdir(dstestdir)
+#os.mkdir(dstestdir)
 
 for i in range(len(df)):
     cur_name = df[i]['name'][0]
@@ -23,12 +24,25 @@ for i in range(len(df)):
     cur_wp = os.path.join(dswavdir, cur_wav)
     cur_test = f'{cur_name}.test'
     cur_tp = os.path.join(dstestdir, cur_test)
-    if os.path.isfile(cur_wp) == False:
-        print(f'{cur_file} not found')
-        break
+    cur_fold = df[i]['fold'][0]
+    fold_folder = f'fold_{cur_fold}'
+    print(cur_name, cur_fold)
+    from_dir = None
+    cur_file = None
+    from_fp = None
+    if mv_test == True:
+        from_dir = dstestdir
+        filename = cur_test
+        from_fp = cur_tp
     else:
-        file = open(cur_tp, 'w')
-        file.close()
-        
+        from_dir = dswavdir
+        filename = cur_wav
+        from_fp = cur_wp
+
+    fold_fp = os.path.join(from_dir, fold_folder)
+    to_fp = os.path.join(fold_fp, filename)
+    if os.path.isdir(fold_fp) == False:
+        os.mkdir(fold_fp)
+    os.rename(from_fp, cur_wp)
 
 
