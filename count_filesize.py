7c97d09c09903b55bd0e3ce4b867b3adcbc87b1a
derekxkwan <derek.x.kwan@gmail.com>
2026-07-25 10:14:06 -0700

    dses

--- count_filesize.py
+++ count_filesize.py
@@ -2,12 +2,10 @@ import librosa as lr
 import os
 import polars as pl
 
-ds = 'polyrhythms'
-#ds = 'dynamics'
+dses = ['polyrhythms', 'dynamics']
 
 home_dir = '/nfs/hpc/share/kwand/syntheory_plus'
 
-load_dir = os.path.join(home_dir, ds)
 
 def get_dynamics_rvb_offset(name):
     cur = fname.split('_')
@@ -24,40 +22,42 @@ def get_polyrhythms_bpm_rvb_offset(name):
     tup = tuple([int(x.split('_')[1]) for x in cur[1:4]])
     return tup
 
-fnames = []
-num_samp = []
-sr = []
-bpm = []
-rvblvl = []
-offset = []
-for f in os.listdir(load_dir):
-    fname = f.split('.')[0]
-    fp = os.path.join(load_dir, f)
-    x, _sr = lr.load(fp, sr=None, mono=False)
-    fnames.append(fname)
-    num_samp.append(x.shape[-1])
-    sr.append(_sr)
-    cur = None
+for ds in dses:
+    load_dir = os.path.join(home_dir, ds)
+    fnames = []
+    num_samp = []
+    sr = []
+    bpm = []
+    rvblvl = []
+    offset = []
+    for f in os.listdir(load_dir):
+        fname = f.split('.')[0]
+        fp = os.path.join(load_dir, f)
+        x, _sr = lr.load(fp, sr=None, mono=False)
+        fnames.append(fname)
+        num_samp.append(x.shape[-1])
+        sr.append(_sr)
+        cur = None
+        if ds == 'polyrhythms':
+            cur = get_polyrhythms_bpm_rvb_offset(fname)
+            bpm.append(cur[0])
+            rvblvl.append(cur[1])
+            offset.append(cur[2])
+        else:
+            cur = get_dynamics_rvb_offset(fname)
+            rvblvl.append(cur[0])
+            offset.append(cur[1])
+
+    data = None
+    schema = None
     if ds == 'polyrhythms':
-        cur = get_polyrhythms_bpm_rvb_offset(fname)
-        bpm.append(cur[0])
-        rvblvl.append(cur[1])
-        offset.append(cur[2])
+        data = {'name': fnames, 'bpm': bpm, 'rvb': rvblvl, 'offset': offset, 'num_samp': num_samp, 'sr': sr}
+        schema = [('name', pl.String), ('bpm', pl.Int64), ('rvb', pl.Int64), ('offset', pl.Int64), ('num_samp', pl.Int64), ('sr', pl.Int64)]
     else:
-        cur = get_dynamics_rvb_offset(fname)
-        rvblvl.append(cur[0])
-        offset.append(cur[1])
+        data = {'name': fnames, 'rvb': rvblvl, 'offset': offset, 'num_samp': num_samp, 'sr': sr}
+        schema = [('name', pl.String), ('rvb', pl.Int64), ('offset', pl.Int64), ('num_samp', pl.Int64), ('sr', pl.Int64)]
 
-data = None
-schema = None
-if ds == 'polyrhythms':
-    data = {'name': fnames, 'bpm': bpm, 'rvb': rvblvl, 'offset': offset, 'num_samp': num_samp, 'sr': sr}
-    schema = [('name', pl.String), ('bpm', pl.Int64), ('rvb', pl.Int64), ('offset', pl.Int64), ('num_samp', pl.Int64), ('sr', pl.Int64)]
-else:
-    data = {'name': fnames, 'rvb': rvblvl, 'offset': offset, 'num_samp': num_samp, 'sr': sr}
-    schema = [('name', pl.String), ('rvb', pl.Int64), ('offset', pl.Int64), ('num_samp', pl.Int64), ('sr', pl.Int64)]
-
-df = pl.DataFrame(data, schema=schema)
-df.write_csv(f'{ds}-counts.csv')
-#print(df)
+    df = pl.DataFrame(data, schema=schema)
+    df.write_csv(f'{ds}-counts.csv')
+    #print(df)
 
