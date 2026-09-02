c7e8dc7a19d0c8f5413a1eaa23080f66b248519c
Derek Kwan <derek.x.kwan@gmail.com>
2026-09-01 22:13:25 -0700

    fix

--- chords7.py
+++ chords7.py
@@ -52,14 +52,11 @@ def transpose_to_range(cur_midinotes, inv_idx):
     transposed_down = False
     cur_root_idx = (4 - inv_idx) % 4
     cur_root = mnc.midi_to_note(cur_midinotes[cur_root_idx], sharp = True)
-    need-to_transpose = False
-    for i in len(cur_midinotes):
-        if cur_midinotes[i] >= midi_upper_limit
-    if cur_midinotes[0] >= midi_upper_limit:
-        transposed_down = True
-        while cur_midinotes[0] >= midi_upper_limit:
-            cur_midinotes = offset_notes(cur_midinotes, -12)
-        cur_root = mnc.midi_to_note(cur_midinotes[cur_root_idx], sharp = True)
+    if cur_midinotes[cur_root_idx] >= midi_upper_limit:
+            transposed_down = True
+            while cur_midinotes[cur_root_idx] >= midi_upper_limit:
+                cur_midinotes = offset_notes(cur_midinotes, -12)
+            cur_root = mnc.midi_to_note(cur_midinotes[cur_root_idx], sharp = True)
     return cur_midinotes, transposed_down, cur_root
 
 def get_outname(chord_type, inv_idx, short_inst, cur_root, ext = ""):
